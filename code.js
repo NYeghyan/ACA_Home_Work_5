2128ba4e37a2d5ca2fa619f66d25011c8a80605a
NYeghyan <nikolayeghyan@gmail.com>
2022-01-31 03:54:45 -0800

    add file

--- code.js
+++ code.js
@@ -18,6 +18,7 @@ function myConcat(arr1, arr2) {
 }
 
 console.log(myConcat(arrOne,arrTwo))
+console.log(arrOne.concat(arrTwo))
 
 
 
@@ -197,7 +198,7 @@ console.log(myReduceFunction(myReduceArr))
 ////// Given an array. Determine whether it consists only of unique items or not. START
 
 const givenArr = [1,2,3,4,4,4,5,6,6,6]
-const gg = [1,2,3,4,5,6,7,8,9]
+const givenArrTwo = [1,2,3,4,5,6,7,8,9]
 
 function findMatch(arr){
     const newArr = []
@@ -205,23 +206,187 @@ function findMatch(arr){
         if(!newArr.includes(arr[i])) {
             newArr.push(arr[i])
         }
-        if(newArr.length === arr.length) {
-            return false
-        }
     }
-    return true
+    if(newArr.length === arr.length) {
+        return ("This array is consists only of unique items")
+    }else {
+        return ("This array is not consists only of unique items")
+    }
 }
 
 
 console.log(findMatch(givenArr))
-console.log(findMatch(gg))
-
+console.log(findMatch(givenArrTwo))
 
 
 ////// Given an array. Determine whether it consists only of unique items or not. END
 
 
 
-let myString = "annasun"
 
 
+/////////// Determine if a word or phrase is an isogram  START
+
+let myString = "theoretically"
+let myStringtwo = "word"
+
+
+
+/////////////// Solution 1
+let isIsogram = (str) => str.split("").every((c, i) => str.indexOf(c) == i);
+
+
+console.log(isIsogram(myString))
+console.log(isIsogram(myStringtwo))
+
+
+
+/////////////// Solution 2
+
+function findDublikate(string){
+    const arr = string.toLowerCase().split("")
+    const myNewArr = []
+    for(let i = 0; i < arr.length; i++) {
+        if(!myNewArr.includes(arr[i])) {
+            myNewArr.push(arr[i])
+        }
+    }
+    if(myNewArr.length === arr.length) {
+        return ("Input word  is isogram ")
+    }else {
+        return ("Input word  is not isogram ")
+    }
+}
+console.log(findDublikate(myString))
+console.log(findDublikate(myStringtwo))
+
+
+/////////// Determine if a word or phrase is an isogram  END
+
+
+
+
+///////////////////////////Lodash  Start
+
+
+
+
+///// intersection Start
+
+
+const myLodashArrOne = [3,1,2,1,4,5,6,6,33]
+const myLodashArrTwo = [7,8,9,10,1,2,3,45,46,78,12,44,33,11,22,6]
+
+
+function intersection(arrone,arrtwo) {
+    const myNewArr = []
+    for(let k = 0; k < arrone.length; k++) {
+        for(let i = 0; i < arrtwo.length; i++){
+            if( arrone[k] === arrtwo[i] ) {
+                if(!myNewArr.includes(arrtwo[i]))
+                    myNewArr.push(arrtwo[i])
+                }
+            }
+        }
+        return myNewArr
+    }
+
+
+console.log(intersection(myLodashArrOne,myLodashArrTwo))
+
+
+
+
+console.log(_.intersection(myLodashArrOne,myLodashArrTwo))
+
+
+///// intersection End
+
+
+///// pull Start
+
+const arrayPull = ['Apple', 'Apricot', 'Banana', 'Avocado','Mango','Plum'];
+ 
+function myPull (array, ... strings) {
+
+    for(let i = 0; i < array.length; i++) {
+        for(let k = 0; k <= strings.length; k++) {
+            if(strings[k] === array[i] ) {
+                array.splice(i, 1)
+            }
+        }
+    }
+    return array
+}
+console.log(myPull(arrayPull,'Apple', 'Banana','Plum'))
+
+
+
+_.pull(arrayPull, 'Apple', 'banana');
+console.log(arrayPull);
+
+
+
+///// pull End
+
+
+
+
+///// Tail Start
+
+
+const arrayTail = ['Lemon', 'Coconut', 'Peach', 'Blueberry'];
+ 
+
+function myTail (arr) {
+    const newTailArr = []
+    for(let i = 1; i < arr.length; i++) {
+        newTailArr.push(arr[i])
+    }
+    return newTailArr
+}
+
+console.log(myTail(arrayTail))
+
+
+console.log(_.tail(arrayTail))
+
+
+
+
+
+
+///// Tail End
+
+
+///// Take Start
+
+
+const arrayTake = ['Mango', 'Cherry', 'Pear', 'Orange'];
+
+
+function myTake(arr, takeKount) {
+    const myNewTakeArr = []
+    if(takeKount === undefined){
+        myNewTakeArr[0] = arr[0]
+    }
+    if(takeKount !== undefined) {
+        for (let i = 0; i < takeKount; i++ ) {
+            myNewTakeArr.push(arr[i])
+        }
+    }
+    return myNewTakeArr
+}
+
+console.log(myTake(arrayTake,3))
+console.log(myTake(arrayTake))
+
+
+
+console.log(_.take(arrayTake, 3))
+console.log(_.take(arrayTake))
+
+///// Take End
+
+
+///////////////////////////Lodash  Start
